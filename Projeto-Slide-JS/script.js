0676521335cb8c60f0ab7baa2302e371e23d57bd
Gustavo Ulian Zanardi <134564393+GustavoZanardi15@users.noreply.github.com>
2025-01-30 13:23:53 -0300

    Update script.js

--- Projeto-Slide-JS/script.js
+++ Projeto-Slide-JS/script.js
@@ -1,29 +1,21 @@
-new Swiper('.card-wrapper', {
-    loop: true,
-    spaceBetween: 30,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+const swiper = new Swiper('.card-wrapper', {
+  loop: true,
+  spaceBetween: 30,
 
-    breakpoints:{
-        0:{
-            slidesPerView: 1
-        },
-        768:{
-            slidesPerView: 2
-        },
-        1024:{
-            slidesPerView: 3
-        },
-    }
-  });
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+      0: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 },
+  }
+});
